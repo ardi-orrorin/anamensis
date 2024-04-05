d9c67c5aa2d4ce921adec366edc6a3e901e559e6
yoosc89 <yoosc89@gmail.com>
2024-04-05 15:21:00 +0900

    1. opt 생성 및 인증 코드 추가

--- server/src/test/java/com/anamensis/server/otp/OTP.java
+++ server/src/test/java/com/anamensis/server/otp/OTP.java
@@ -1,35 +1,46 @@
 package com.anamensis.server.otp;
 
 
-import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.security.Keys;
-import io.vertx.ext.auth.otp.OtpCredentials;
-import io.vertx.ext.auth.otp.OtpKey;
-import io.vertx.ext.auth.otp.totp.TotpAuth;
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
+import com.warrenstrange.googleauth.GoogleAuthenticatorQRGenerator;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import javax.crypto.SecretKey;
 import java.util.Base64;
 
 @SpringBootTest
 public class OTP {
 
+    Logger log = org.slf4j.LoggerFactory.getLogger(OTP.class);
+
     @Test
-    void name() throws Exception {
+    void name() {
 
-        TotpAuth totpAuth = TotpAuth.create();
-        SecretKey secret = Keys.secretKeyFor(SignatureAlgorithm.HS512);
-        Base64.Encoder encoder = Base64.getEncoder();
-        OtpKey otpKey = new OtpKey();
-        otpKey.setAlgorithm("SHA512");
-        otpKey.setKey(encoder.encodeToString(secret.getEncoded()));
-        String scret = totpAuth.generateUri(otpKey, "isUser", "user");
-        System.out.println(scret);
+//        TotpAuth totpAuth = TotpAuth.create();
+//        SecretKey secret = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+//        Base64.Encoder encoder = Base64.getEncoder();
+//        OtpKey otpKey = new OtpKey();
+//        otpKey.setAlgorithm("SHA512");
+//        otpKey.setKey(encoder.encodeToString(secret.getEncoded()));
+//        String scret = totpAuth.generateUri(otpKey, "isUser", "user");
+//        System.out.println(scret);
+//
+//        OtpCredentials otpCredentials =
+//                new OtpCredentials(encoder.encodeToString(secret.getEncoded()), "SHA512");
 
-        OtpCredentials otpCredentials =
-                new OtpCredentials(encoder.encodeToString(secret.getEncoded()), "SHA512");
-        otpCredentials.checkValid("123456");
+
+        GoogleAuthenticator gAuth = new GoogleAuthenticator();
+        GoogleAuthenticatorKey key = gAuth.createCredentials();
+
+        System.out.println("Verification code is: " + key.getVerificationCode());
+
+        String qrcode = GoogleAuthenticatorQRGenerator.getOtpAuthTotpURL("amanesis","test", key);
+
+        System.out.println("qrcode: " + qrcode);
+
+        System.out.println("Is valid: " + gAuth.authorize(key.getKey(), 123456));
 
     }
 }

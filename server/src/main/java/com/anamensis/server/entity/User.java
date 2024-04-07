1f06ea8842a88d6ea11e44056be6cb4f2d5d829d
yoosc89 <yoosc89@gmail.com>
2024-04-07 17:03:52 +0900

    1. name unique 속성 삭제 2. 계정 생성시 role User 생성 3. api Req, Res 구체화 4. user insert 시 id값 반환하도록 mapper 수정

--- server/src/main/java/com/anamensis/server/entity/User.java
+++ server/src/main/java/com/anamensis/server/entity/User.java
@@ -20,21 +20,14 @@ public class User{
 
     private long id;
 
-    @NotNull(message = "User ID is required")
     private String userId;
 
-    @NotNull(message = "Password is required")
     private String pwd;
 
-    @NotNull(message = "Name is required")
     private String name;
 
-    @NotNull(message = "Email is required")
-    @Pattern(regexp = "^[a-zA-Z0-9]+@[a-zA-Z0-9]+\\.[a-zA-Z0-9]+$", message = "Invalid email")
     private String email;
 
-    @NotNull(message = "Phone number is required")
-    @Pattern(regexp = "^01(?:0|1|[6-9])-(?:\\d{3}|\\d{4})-\\d{4}$", message = "Invalid phone number")
     private String phone;
 
     private long point;

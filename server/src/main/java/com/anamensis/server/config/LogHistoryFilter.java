b689924f5d2b061e369779517a94e6d4458ced2a
yoosc89 <yoosc89@gmail.com>
2024-04-09 15:22:07 +0900

    1. 주석 추가

--- server/src/main/java/com/anamensis/server/config/LogHistoryFilter.java
+++ server/src/main/java/com/anamensis/server/config/LogHistoryFilter.java
@@ -9,7 +9,6 @@ import org.springframework.core.io.buffer.DataBuffer;
 import org.springframework.core.io.buffer.DataBufferFactory;
 import org.springframework.core.io.buffer.DataBufferUtils;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpRequestDecorator;
 import org.springframework.stereotype.Component;
@@ -25,6 +24,10 @@ import java.nio.charset.StandardCharsets;
 import java.security.Principal;
 import java.time.LocalDateTime;
 
+
+/**
+ * 로그인 한 유저의 API 호출 로그 기록 필터
+ */
 @Slf4j
 @Component
 @RequiredArgsConstructor
@@ -38,11 +41,22 @@ public class LogHistoryFilter implements WebFilter {
     public Mono<Void> filter(ServerWebExchange exchange, WebFilterChain chain) {
         String path = exchange.getRequest().getPath().toString();
         HttpMethod method = exchange.getRequest().getMethod();
+        String userRegexp = "/user/\\S+";
 
-        if(path.equals("/user/login") || path.equals("/user/signup")) {
+        // /user/histories는 제외한 모든 /user/** 경로에 대해 로그를 남김
+        if(path.matches(userRegexp) && !path.equals("/user/histories")) {
             return chain.filter(exchange);
         }
 
+        //--------------------------------------------------------------------------------
+        // 밑의 로직이 실행 되기 위해서는 Spring Security에서 유저가 인가되어야만 사용 가능
+        //--------------------------------------------------------------------------------
+
+        // RequestBody의 사용 기준으로 분기 처리 (DataBuffer를 byte[]로 변환)
+        // getBody의 DataBuffer를 byte[]로 변환하여 로그에 기록하는데,
+        // 이 때 DataBuffer를 release하지 않으면 메모리 누수가 발생할 수 있음
+
+        // GET, DELETE는 body가 없이 로그 기록
         if(HttpMethod.GET.equals(method) || HttpMethod.DELETE.equals(method)) {
             return Mono.zip(Mono.just(exchange), exchange.getPrincipal(), exchange.getSession())
                     .doOnNext(t -> logWrite(new byte[0], t.getT1(),(Principal) t.getT2(), t.getT3()))
@@ -50,13 +64,17 @@ public class LogHistoryFilter implements WebFilter {
                     .flatMap(chain::filter);
         }
 
+        // PUT, POST, PATCH는 body가 있을 때 로그 기록
+        // body를 byte[]로 변환하여, String 형태로 변환하여 로그에 기록
         if(HttpMethod.PUT.equals(method) || HttpMethod.POST.equals(method) || HttpMethod.PATCH.equals(method)) {
             Flux<DataBuffer> body = exchange.getRequest().getBody();
 
             return Mono.zip(DataBufferUtils.join(body), exchange.getPrincipal(), exchange.getSession())
-                    .map(t -> t.mapT1(this::toBytes))
+                    .map(t -> t.mapT1(this::toBytes)) // DataBuffer -> byte[]
                     .doOnNext(t -> logWrite(t.getT1(), exchange, (Principal) t.getT2(), t.getT3()))
                     .map(t -> newRequest(t.getT1(), exchange))
+                    // DataBuffer에서 기록을 위해 byte[]로 변환하여 이벤트를 사용 했으므로,
+                    // 다시 byte[]를 DataBuffer로 변환하여 다음 필터를 진행하도록 함
                     .flatMap(chain::filter);
         }
 

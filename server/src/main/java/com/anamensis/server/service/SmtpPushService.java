3e39eabb4464169b7f875a9dd629b21801008bcd
yoosc89 <yoosc89@gmail.com>
2024-04-10 18:07:24 +0900

    1. smtp 발송 코드 추가

---
+++ server/src/main/java/com/anamensis/server/service/SmtpPushService.java
@@ -0,0 +1,40 @@
+package com.anamensis.server.service;
+
+import com.anamensis.server.entity.SmtpPush;
+import com.anamensis.server.entity.UserConfigSmtp;
+import com.anamensis.server.mapper.SmtpPushMapper;
+import com.anamensis.server.provider.MailProvider;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@Service
+public class SmtpPushService {
+
+    private final SmtpPushMapper smtpPushMapper;
+
+    public Mono<Boolean> send(UserConfigSmtp userConfigSmtp, SmtpPush smtpPush) {
+        return Mono.just(smtpPush)
+                .doOnNext(smtp -> {
+                    smtp.setUserConfigSmtpPk(userConfigSmtp.getId());
+                    smtp.setUserPk(userConfigSmtp.getUserPk());
+                    smtp.setCreateAt(LocalDateTime.now());
+                })
+                .zipWith(Mono.just(userConfigSmtp))
+                .doOnNext(t -> {
+                    new MailProvider.Builder()
+                            .config(t.getT2())
+                            .message(t.getT2(), t.getT1().getSubject(), t.getT1().getContent())
+                            .build()
+                            .send();
+                })
+                .map(t -> {
+                    smtpPushMapper.save(t.getT1());
+                    return true;
+                });
+    }
+}

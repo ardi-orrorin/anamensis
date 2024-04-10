28bf9c55c59596eb0f6ee275374db8eece13dc72
yoosc89 <yoosc89@gmail.com>
2024-04-10 13:18:59 +0900

    1. refresh token 코드 추가

--- server/src/main/java/com/anamensis/server/provider/TokenProvider.java
+++ server/src/main/java/com/anamensis/server/provider/TokenProvider.java
@@ -1,11 +1,9 @@
 package com.anamensis.server.provider;
 
 import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.JwtParser;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 

5c6ec68654fa684e35066d1e16fcdbd989edd2d6
yoosc89 <yoosc89@gmail.com>
2024-04-11 18:33:32 +0900

    1. docker compose 내용 추가

--- build.sh
+++ build.sh
@@ -2,13 +2,13 @@ echo .
 
 version=$1
 
-echo 'start springboot build'
+echo 'start anamensis build'
 gradle build ':server:build' -Pversion=${version}
 
 echo 'docker build '+${version}
-docker build --build-arg='JAR=server-anamensis-'${version} -t springboot-docker:${version}  .
+docker build --build-arg='JAR=server-anamensis-'${version} -t anamensis:${version}  .
 
 echo 'docker build latest'
-docker build --build-arg='JAR=server-anamensis-'${version} -t springboot-docker:latest  .
+docker build --build-arg='JAR=server-anamensis-'${version} -t anamensis:latest  .
 
-docker run -it -p 18080:8080 springboot-docker:${version}
+docker-compose up -d

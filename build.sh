8cb2024fa62ed758106c22f4e715dc0110234932
yoosc89 <yoosc89@gmail.com>
2024-04-12 01:12:14 +0900

    1. docker-compose 빌드 내용 추가

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-echo .
+clear
+
+ehoc build start....
 
 version=$1
 file_name='server-anamensis-'${version}''
@@ -9,16 +11,14 @@ then
     exit 1
 fi
 
-echo 'start anamensis build'
+echo 'start anamensis build...'
+
 gradle build ':server:build' -Pversion=${version}
 
-echo 'docker build '+${version}
-docker build --build-arg='JAR='${file_name} -t anamensis:${version}  .
 
-echo 'docker build latest'
-docker build --build-arg='JAR='${file_name} -t anamensis:latest  .
+echo 'docker build start....'
 
-#docker push username/anamensis:${version}
-#docker push username/anamensis:latest
-
+TAG=${version} docker-compose build
 TAG=${version} docker stack deploy -c docker-compose.yml anamensis
+
+echo 'build success....'

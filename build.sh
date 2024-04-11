37082f8f922a52421abdc7806bcfeba7862a7900
yoosc89 <yoosc89@gmail.com>
2024-04-12 02:25:32 +0900

    1. 헬스체크 보완

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 clear
 
-ehoc build start....
+ehoc proejct build start....
+echo
 
 version=$1
+port=$2
 file_name='server-anamensis-'${version}''
 
 if((!version))
@@ -11,14 +13,43 @@ then
     exit 1
 fi
 
-echo 'start anamensis build...'
+echo 'start anamensis proejct build start....'
 
 gradle build ':server:build' -Pversion=${version}
 
+echo 'start anamensis proejct build success....'
+
+echo
 
 echo 'docker build start....'
 
+echo
+
+echo 'docker latest build start....'
+
+docker build --build-arg='JAR='${file_name} -t anamensis:latest  .
+
+echo 'docker latest build success....'
+
+echo
+
+
+echo 'docker-compose build start....'
+
 TAG=${version} docker-compose build
-TAG=${version} docker stack deploy -c docker-compose.yml anamensis
+
+echo 'docker-compose build success....'
+
+ehoc 'docker build success....'
+
+echo
+
+echo 'docker stack deploy start....'
+
+TAG=${version} PORT=${port} docker stack deploy -c docker-compose.yml anamensis
+
+echo 'docker stack deploy success....'
+
+echo
 
 echo 'build success....'

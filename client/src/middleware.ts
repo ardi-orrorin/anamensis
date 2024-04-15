78b1a23b86fa0c0e2e79ae9a6a5e81e33175f322
yoosc89 <yoosc89@gmail.com>
2024-04-15 14:38:17 +0900

    1. refresh token 활용 코드 추가

--- client/src/middleware.ts
+++ client/src/middleware.ts
@@ -1,10 +1,28 @@
 import {NextRequest, NextResponse} from "next/server";
 import {cookies} from "next/headers";
+import {RequestCookie} from "next/dist/compiled/@edge-runtime/cookies";
 
 export async function middleware(req: NextRequest) {
-    // const isLogged = cookies().get('accessToken') !== undefined && cookies().get('refreshToken') !== undefined;
-    const isLogged = cookies().get('accessToken') !== undefined;
+    const isLogged = cookies().get('accessToken');
+
     const url = req.nextUrl.clone();
+
+    const refreshToken = cookies().get('refreshToken');
+
+
+    if(!isLogged && refreshToken) {
+        const result = await generateRefreshToken(refreshToken);
+
+        const next = NextResponse.next();
+        next.cookies.set('accessToken', result.accessToken, {
+            secure: result.secure,
+            path: result.Path,
+            maxAge: new Date(result.Expires).getMinutes()
+        });
+
+        return next;
+
+    }
 
     if (!isLogged) {
         url.pathname = '/';
@@ -14,6 +32,42 @@ export async function middleware(req: NextRequest) {
 
     return NextResponse.next();
 }
+
+const generateRefreshToken = async (refreshToken: RequestCookie): Promise<AccessCookieI> => {
+    const refresh = await fetch(process.env.NEXT_PUBLIC_SERVER + '/user/refresh', {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${refreshToken.value}`
+        }
+    });
+
+    const accessToken = refresh.headers.getSetCookie().find((cookie: string) => {
+        const [key, value] = cookie.split(';')[0].split('=');
+        if(key === 'accessToken') {
+            return cookie;
+        }
+    });
+
+    // @ts-ignore
+    const token: AccessCookieI = accessToken!.split(';')?.map((cookie: string) => {
+        const [key, value] = cookie.split('=');
+        return {[key] : value};
+    }).reduce((acc, curr) => {
+        return {...acc, ...curr};
+    });
+
+    // @ts-ignore
+    return token;
+}
+
+export interface AccessCookieI {
+    accessToken: string;
+    Expires: string;
+    secure: boolean | undefined;
+    Path: string;
+    path: string;
+}
+
 
 export const config = {
     matcher: [

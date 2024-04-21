b8d0ffbf0cffbf079de46b388939e03e0511a06f
yoosc89 <yoosc89@gmail.com>
2024-04-21 14:48:41 +0900

    1. batch project 추가

--- server/src/main/java/com/anamensis/server/config/PathRequestMappingConfig.java
+++ server/src/main/java/com/anamensis/server/config/PathRequestMappingConfig.java
@@ -1,33 +1,25 @@
 package com.anamensis.server.config;
 
 import com.anamensis.server.controller.PublicAPI;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.reactive.result.method.RequestMappingInfo;
 import org.springframework.web.reactive.result.method.annotation.RequestMappingHandlerMapping;
 
 import java.lang.reflect.Method;
-import java.util.Set;
 
-@Slf4j
 public class PathRequestMappingConfig extends RequestMappingHandlerMapping {
 
     @Override
     protected RequestMappingInfo getMappingForMethod(Method method, Class<?> handlerType) {
+        if(method.getAnnotation(PublicAPI.class) == null) return super.getMappingForMethod(method, handlerType);
 
         RequestMappingInfo requestMappingInfo = super.getMappingForMethod(method, handlerType);
 
-        if(method.getAnnotation(PublicAPI.class) != null) {
-            Set<String> paths = requestMappingInfo.getDirectPaths();
-
-            String path = String.join("/", paths);
-
-            RequestMappingInfo.Builder builder = RequestMappingInfo.paths("public" + path);
-
-            log.info("Public API: {}", builder.build().getDirectPaths());
-
-            return builder.build();
+        if(requestMappingInfo != null && method.getAnnotation(PublicAPI.class) != null) {
+            return RequestMappingInfo.paths("public")
+                    .build()
+                    .combine(requestMappingInfo);
         }
 
-        return super.getMappingForMethod(method, handlerType);
+        return requestMappingInfo;
     }
 }

756d853ec5ed0c7f97bf768020f520f71f08a74b
yoosc89 <yoosc89@gmail.com>
2024-05-12 17:02:51 +0900

    1. block input style 내용 추가

---
+++ client/src/app/{components}/block/input/InputBase.tsx
@@ -0,0 +1,52 @@
+import {BlockProps} from "@/app/{components}/block/type/Types";
+import React, {CSSProperties, DetailedHTMLProps, InputHTMLAttributes} from "react";
+
+export default function InputBase({
+   data, style
+}:{
+    data: BlockProps,
+    style: CSSProperties
+}){
+
+    const {
+        onKeyUpHandler,
+        onKeyDownHandler, onChangeHandler,
+        value, isView,
+        size, color, bg,
+        blockRef, seq
+    } = data;
+
+    const defaultBg = {
+        input: 'rgba(230,230,230,0.2)',
+        span: ''
+    }
+
+    const customStyle: CSSProperties = {
+        outline: 'none',
+        border: 'none',
+        width: '100%',
+        fontSize: size ?? style.fontSize ?? '1rem',
+        color: color ?? 'black',
+        fontWeight: style.fontWeight,
+        padding: style.padding,
+        letterSpacing: style.letterSpacing,
+    };
+
+    const props: DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> = {
+        ref: el => blockRef.current[seq] = el,
+        onKeyUp: onKeyUpHandler,
+        onKeyDown: onKeyDownHandler,
+        onChange: onChangeHandler,
+        value,
+    }
+
+    return (
+        <div style={{display: 'flex', width: '100%', padding: '0.5rem'}}>
+            {
+                isView
+                ? <span style={{...customStyle, backgroundColor: bg || defaultBg.span}} {...props}>{value}</span>
+                : <input style={{...customStyle, backgroundColor: bg || defaultBg.input}} {...props}/>
+            }
+        </div>
+    )
+}
--- client/src/app/{components}/block/type/Types.ts
+++ client/src/app/{components}/block/type/Types.ts
@@ -1,22 +1,27 @@
 import {ChangeEvent, Dispatch, MutableRefObject, SetStateAction} from "react";
 
 export type BlockProps = {
-    seq: number;
+    seq                : number;
+    color?             : string;
+    code               : string;
+    bg?                : string;
+    text?              : string;
+    size?              : string;
+    isView             : boolean;
+    openMenu           : boolean;
+    value              : string;
+    setValue           : Dispatch<SetStateAction<string>>;
+    blockRef           : MutableRefObject<HTMLElement[] | null[]>;
+    openMenuToggle     : ({label, code}: MenuParams) => void;
+    onChangeHandler    : (e: ChangeEvent<HtmlElements>) => void;
+    onKeyUpHandler     : (e: React.KeyboardEvent<HtmlElements>) => void;
+    onKeyDownHandler   : (e: React.KeyboardEvent<HtmlElements>) => void;
+    onClickAddHandler  : () => void;
+}
+
+export type MenuParams = {
+    label: string;
     code: string;
-    color: string;
-    bg : string;
-    text: string;
-    size: string;
-    isView: boolean;
-    openMenu: boolean;
-    openMenuToggle: () => void;
-    onChangeHandler: (e: ChangeEvent<HtmlElements>) => void;
-    onKeyUpHandler: (e: React.KeyboardEvent<HtmlElements>) => void;
-    onKeyDownHandler: (e: React.KeyboardEvent<HtmlElements>) => void;
-    onClickAddHandler: () => void;
-    value: string;
-    setValue: Dispatch<SetStateAction<string>>;
-    blockRef: MutableRefObject<HTMLElement[] | null[]>;
 }
 
 export type HtmlElements = HTMLInputElement | HTMLTextAreaElement | HTMLButtonElement | HTMLDataElement;

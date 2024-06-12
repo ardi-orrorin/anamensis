60b9bd68d0bb14731413b2aa5f73d29d222b9acb
yoosc89 <yoosc89@gmail.com>
2024-06-13 00:55:09 +0900

    1. nextjs build 오류 수정

--- client/src/app/api/find-user/id-confirm/route.ts
+++ client/src/app/api/find-user/id-confirm/route.ts
@@ -1,12 +1,11 @@
 import {NextRequest} from "next/server";
-import {SaveComment} from "@/app/board/[id]/{components}/comment";
 import apiCall from "@/app/{commons}/func/api";
-import {FindUser} from "@/app/find-user/page";
+import {FindUser} from "@/app/reset-pwd/{services}/type";
 
 export async function POST(req: NextRequest){
     const body: FindUser  = await req.json();
 
-    const result = await apiCall<boolean, SaveComment>({
+    const result = await apiCall<boolean, FindUser>({
         path: '/public/api/user/find-id-email-confirm',
         method: 'POST',
         body,

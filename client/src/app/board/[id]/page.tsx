2e609de85af07f5684aee8aec36ae67ba45b947c
yoosc89 <yoosc89@gmail.com>
2024-07-10 10:05:30 +0900

    1. 단축키 변경

--- client/src/app/board/[id]/page.tsx
+++ client/src/app/board/[id]/page.tsx
@@ -2,7 +2,6 @@
 
 import Block from "@/app/board/{components}/Block";
 import {ChangeEvent, useCallback, useContext, useEffect, useMemo, useRef, useState} from "react";
-import GlobalLoadingSpinner from "@/app/{commons}/GlobalLoadingSpinner";
 import {HtmlElements} from "@/app/board/{components}/block/type/Types";
 import {BlockI, BoardI, Category} from "@/app/board/{services}/types";
 import {blockTypeList} from "@/app/board/{components}/block/list";
@@ -25,8 +24,8 @@ import {deleteImage, initBlock, listSort, notAvailDupCheck, updateBoard} from "@
 import WriterInfo from "@/app/board/[id]/{components}/writerInfo";
 import {useRouter} from "next/navigation";
 import {useHotkeys} from "react-hotkeys-hook";
-import HotKeybtn from "@/app/{components}/hotKeybtn";
 import {Options} from "react-hotkeys-hook/src/types";
+import HotKeyInfo from "@/app/board/[id]/{components}/hotKeyInfo";
 
 export interface RateInfoI {
     id      : number;
@@ -86,7 +85,6 @@ export default function Page({params}: {params : {id: string}}) {
 
         return block;
     };
-
     const validation = useCallback(() => {
         const title = board.data.title !== '';
         const content = board.data.content.list.filter(item => item.value !== '').length > 0;
@@ -395,81 +393,9 @@ export default function Page({params}: {params : {id: string}}) {
         <div className={'p-5 flex flex-col gap-5 justify-center items-center'}>
             <div className={`relative w-full flex flex-col gap-6 duration-700 ${fullScreen || 'lg:w-2/3 xl:w-3/5'}`}>
                 <div className={[`absolute z-10 top-1/4 -right-52 hidden`, fullScreen ? 'lg:hidden' : 'lg:block'].join(' ')}>
-                    <div className={'flex flex-col w-48 px-2 py-4 text-xs shadow rounded  bg-gray-50'}>
-                        <h1 className={'flex justify-center p-2'}>
-                            단축키
-                        </h1>
-                        <ul className={'w-full flex flex-col gap-2 justify-center items-center'}>
-                            <li className={'w-full px-5 justify-between flex gap-2 items-center'}>
-                                <span>
-                                    너비 변경 : 
-                                </span> 
-                                <HotKeybtn hotkey={['SHIFT', 'F']} />
-                            </li>
-                            {
-                                (isNewBoard || !board.isView)
-                                && <>
-                                <li className={'w-full px-5 justify-between flex gap-2 items-center'}>
-                                    <span>
-                                        h1블록 :
-                                    </span>
-                                    <HotKeybtn hotkey={['CTRL', '1']} />
-                                </li>
-                                <li className={'w-full px-5 justify-between flex gap-2 items-center'}>
-                                    <span>
-                                        h2블록 :
-                                    </span>
-                                    <HotKeybtn hotkey={['CTRL', '2']} />
-                                </li>
-                                <li className={'w-full px-5 justify-between flex gap-2 items-center'}>
-                                    <span>
-                                        h3블록 :
-                                    </span>
-                                    <HotKeybtn hotkey={['CTRL', '3']} />
-                                </li>
-                                <li className={'w-full px-5 justify-between flex gap-2 items-center'}>
-                                    <span>
-                                        h4블록 :
-                                    </span>
-                                    <HotKeybtn hotkey={['CTRL', '4']} />
-                                </li>
-                                <li className={'w-full px-5 justify-between flex gap-2 items-center'}>
-                                    <span>
-                                        h5블록 :
-                                    </span>
-                                  <HotKeybtn hotkey={['CTRL', '5']} />
-                                </li>
-                                <li className={'w-full px-5 justify-between flex gap-2 items-center'}>
-                                    <span>
-                                         인용 블록:
-                                    </span>
-                                  <HotKeybtn hotkey={['CTRL', '6']} />
-                                </li>
-                                <li className={'w-full px-5 py-2 justify-center flex items-center'}>
-                                     폰트 스타일
-                                </li>
-                                <li className={'w-full px-5 justify-between flex gap-2 items-center'}>
-                                    <span>
-                                        굵게 :
-                                    </span>
-                                  <HotKeybtn hotkey={['CTRL', 'B']} />
-                                </li>
-                                <li className={'w-full px-5 justify-between flex gap-2 items-center'}>
-                                    <span>
-                                        이탤릭체 :
-                                    </span>
-                                  <HotKeybtn hotkey={['CTRL', 'I']} />
-                                </li>
-                                <li className={'w-full px-5 justify-between flex gap-2 items-center'}>
-                                    <span>
-                                        취소선 :
-                                    </span>
-                                  <HotKeybtn hotkey={['CTRL', ';']} />
-                                </li>
-                                </>
-                            }
-                        </ul>
-                    </div>
+                    <HotKeyInfo isNewBoard={isNewBoard}
+                                isView={board?.isView}
+                    />
                 </div>
                 <div className={'flex h-8 border-l-8 border-solid border-gray-500 px-2 items-center'}>
                     <span className={'font-bold'}>

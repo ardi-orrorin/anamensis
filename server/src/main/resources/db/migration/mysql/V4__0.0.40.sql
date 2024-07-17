7466f344878da467754dbf28a04052f048509888
yoosc89 <yoosc89@gmail.com>
2024-07-18 00:37:56 +0900

    1. 일부 페이지 조회 속도 향상

--- server/src/main/resources/db/migration/mysql/V4__0.0.40.sql
+++ server/src/main/resources/db/migration/mysql/V4__0.0.40.sql
@@ -1,3 +1,4 @@
 CREATE INDEX idx_id_desc_use ON board(id DESC , is_use);
 CREATE INDEX idx_id_desc_use_title ON board(id DESC , is_use, title);
 CREATE INDEX idx_id_desc_public_use ON board(id desc, is_use, is_public);
+CREATE INDEX idx_board_pk_use ON board_comment(board_pk, is_use);

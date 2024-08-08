1b0000d648c3716222687ac02c709cf5a0f1b9f7
yoosc89 <yoosc89@gmail.com>
2024-08-08 13:25:09 +0900

    1. 스케쥴 게시블 컴포넌트 행간 위치 안 맞는 내용 수정

--- client/src/app/{components}/boards/calender.tsx
+++ client/src/app/{components}/boards/calender.tsx
@@ -32,14 +32,14 @@ const CalenderComponent = (props: BoardListI) => {
 
                                 return (
                                     <tr key={'event' + eventBlock.hash + index}
-                                        className={'w-full h-6'}
+                                        className={'w-full flex h-6'}
                                     >
-                                        <td className={'flex gap-1'}>
-                                            <span className={'flex w-20'}>{start}</span>
-                                            <span className={'flex w-5 justify-start'}>~</span>
-                                            <span className={'flex w-20'}>{end}</span>
+                                        <td className={'flex gap-1 items-center'}>
+                                            <span className={'flex w-20 h-full items-center'}>{start}</span>
+                                            <span className={'flex w-5 h-full justify-start items-center'}>~</span>
+                                            <span className={'flex w-20 h-full items-center'}>{end}</span>
                                         </td>
-                                        <td className={'font-bold'}>
+                                        <td className={'flex min-w-full font-bold items-center'}>
                                             {eventBlock.value}
                                         </td>
                                     </tr>

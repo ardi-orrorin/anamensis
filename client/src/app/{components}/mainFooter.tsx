08f40a656f91321d70fd3ff9b2b2814398ce28b9
yoosc89 <yoosc89@gmail.com>
2024-08-26 00:19:33 +0900

    푸터 내용 변경

--- client/src/app/{components}/mainFooter.tsx
+++ client/src/app/{components}/mainFooter.tsx
@@ -1,14 +1,14 @@
+import Link from "next/link";
+
 export default function Footer() {
     return (
-        <div className={'h-14 max-h-14 min-h-14 flex flex-col gap-0.5 justify-center items-center bg-blue-400 text-white text-xs sm:text-sm'}
+        <div className={'h-16 max-h-16 min-h-14 flex flex-col gap-0.5 justify-center items-center bg-blue-400 text-white text-xs sm:text-sm'}
              data-testid={'footer'}
         >
             <span>
-                Copyright © 2024 Anamensis All Rights Reserved.
+                Copyright © 2024 Ardi All Rights Reserved. Ver.{process.env.NEXT_PUBLIC_VERSION || '0.0.0'}
             </span>
-            <span>
-                Anamensis Version : {process.env.NEXT_PUBLIC_VERSION || '0.0.0'}
-            </span>
+            <Link href={'https://github.com/ardi-orrorin'}>GITHUB</Link>
         </div>
     )
 }
